9aa573b7f6566d9c161a68c2e6579f9830d71dc6
Chaitali Mahato <chaitalimahato9497@gmail.com>
2025-12-11 21:11:07 +0530

    home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,56 @@
+import "./Home.css";
+import { useState } from "react";
+import { booksData } from "../Utils/data";
+import BookCard from "../Components/BookCard";
+
+ function Home() {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [showAll, setShowAll] = useState(false);
+
+  const categories = ["All", "Fiction", "Classic", "Sci-Fi", "Romance"];
+
+  const filteredBooks =
+    selectedCategory === "All"
+      ? booksData
+      : booksData.filter((b) => b.category === selectedCategory);
+
+  return (
+    <div className="home">
+
+      <h2 className="title">A smarter way to explore books — all in one place.</h2>
+        <p className="sub">Discover, track, and save your favorite reads effortlessly</p>
+
+      <h2 className="sub-title">Categories</h2>
+      <div className="categories">
+        {categories.map((cat) => (
+          <p
+            key={cat}
+            onClick={() => {
+              setSelectedCategory(cat);
+              setShowAll(false);
+            }}
+            className={selectedCategory === cat ? "active" : ""}
+          >
+            {cat}
+          </p>
+        ))}
+      </div>
+
+      <h2 className="section-title">Popular Books</h2>
+
+      <div className="book-list">
+        {(showAll ? filteredBooks : filteredBooks.slice(0, 6)).map((b) => (
+          <BookCard key={b.id} book={b} />
+        ))}
+      </div>
+
+      {filteredBooks.length > 6 && (
+        <button className="browse-btn" onClick={() => setShowAll(!showAll)}>
+          {showAll ? "Show Less" : "Browse More"}
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default Home;
